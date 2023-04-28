4f575f6d532c4100abd179b427e1e86dc98e7ebf
leonardoss-alencar <leonardo.souza.santosalencar@gmail.com>
2023-04-28 16:15:41 -0300

    iniciando o projeto de Padrões de Projeto

---
+++ src/test/java/com/padroes/projetos/carteira/CarteiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.padroes.projetos.carteira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarteiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
